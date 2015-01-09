03408e58e6d20f712bffad76b7e1046980302936
lyuehh <lyuehh@gmail.com>
2015-01-09 18:56:01 +0800

    auto update

--- lottery/shuang_se_qiu.sh
+++ lottery/shuang_se_qiu.sh
@@ -7,7 +7,8 @@ mv shuang_se_qiu.csv shuang_se_qiu.csv.bak
 
 echo 'id,date,red1,red2,red3,red4,red5,red6,blue1' > shuang_se_qiu.csv
 
-for i in `seq 2003 2015`
+y=`date +%Y`
+for i in `seq 2003 ${y}`
 do
     curl -s  "http://www.3dcp.cn/zs/gonggao.php?type=ssq&year=$i" |  iconv -f gbk -t utf8  | hquery -p -r '$("table").eq(13).parent().html()' | hquery -p -f shuang.js | grep -v '^$' >> shuang_se_qiu.csv
 done

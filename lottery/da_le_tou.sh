54cec3e76bc634d2054c82b366c00dd46e82ddfd
lyuehh <lyuehh@gmail.com>
2015-01-09 18:45:19 +0800

    happy new year

--- lottery/da_le_tou.sh
+++ lottery/da_le_tou.sh
@@ -7,7 +7,7 @@ fi
 mv da_le_tou.csv da_le_tou.csv.bak
 echo 'id,date,red1,red2,red3,red4,red5,blue1,blue2' > da_le_tou.csv
 
-for i in `seq 2007 2014`
+for i in `seq 2007 2015`
 do
     curl -s  "http://www.3dcp.cn/zs/gonggao.php?type=dlt&year=$i" |  iconv -f gbk -t utf8  | hquery -p -r '$("table").eq(13).parent().html()' | hquery -p -f da.js | grep -v '^$' >> da_le_tou.csv
 done
